1f84b1898a37ed7a685dffd92913e52bcae30bd9
Diệp Anh Nguyễn <diepanhnguyen2807@gmail.com>
2024-11-05 14:01:44 +0700

    footer động

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -8,11 +8,21 @@ fetch("navbar.html")
         // }
     })
 
-    fetch("topnav.html")
+fetch("topnav.html")
     .then((response) => response.text())
     .then((html) => {
         const topnav = document.getElementById("topnav");
         topnav.innerHTML = html;
+        // if (idPage) {
+        //     document.querySelector(`#${idPage}`).classList.add("active");
+        // }
+    })
+
+fetch("footer.html")
+    .then((response) => response.text())
+    .then((html) => {
+        const footer = document.getElementById("footer");
+        footer.innerHTML = html;
         // if (idPage) {
         //     document.querySelector(`#${idPage}`).classList.add("active");
         // }
