17337cb74acba335a6749b465291d8fb695d4e4e
Diệp Anh Nguyễn <diepanhnguyen2807@gmail.com>
2024-11-05 12:40:23 +0700

    topnav động

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -7,3 +7,13 @@ fetch("navbar.html")
         //     document.querySelector(`#${idPage}`).classList.add("active");
         // }
     })
+
+    fetch("topnav.html")
+    .then((response) => response.text())
+    .then((html) => {
+        const topnav = document.getElementById("topnav");
+        topnav.innerHTML = html;
+        // if (idPage) {
+        //     document.querySelector(`#${idPage}`).classList.add("active");
+        // }
+    })
